a13b15442244718e0a95b8c6d57e4a2996f39feb
minitaste <nazar.voyevoda@gmail.com>
2024-12-22 22:09:20 +0200

    add tests to check passengers

--- lecture4/airline/flights/tests.py
+++ lecture4/airline/flights/tests.py
@@ -70,3 +70,27 @@ class FlightTestCase(TestCase):
         response = c.get(f"/flights/{max_id + 1}")
         self.assertEqual(response.status_code, 404)
  
+        
+    def test_flight_page_passengers(self):
+        f = Flight.objects.get(pk=1)
+        p = Passenger.objects.create(first="Mkas", last="Lox")
+        f.passengers.add(p)
+
+        c = Client()
+        response = c.get(f"/flights/{f.id}")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["passengers"].count(), 1)
+
+        
+    def test_flight_page_non_passengers(self):
+        f = Flight.objects.get(pk=1)
+        p = Passenger.objects.create(first="Mkas", last="Lox")
+
+        c = Client()
+        response = c.get(f"/flights/{f.id}")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["non_passengers"].count(), 1)
+
+
+
+        
